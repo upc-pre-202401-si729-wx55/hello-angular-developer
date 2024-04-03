ac3ea03d21ef26c6a17edd10ffcd1aae7eebfea3
Angel Velasquez <pcisavel@upc.edu.pe>
2024-04-03 12:41:26 -0500

    feat(welcome): added welcome component with template and behavior.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterDeveloperComponent } from './welcoming/components/register-developer/register-developer.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import { WelcomeComponent } from './welcoming/components/welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterDeveloperComponent
+    RegisterDeveloperComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
