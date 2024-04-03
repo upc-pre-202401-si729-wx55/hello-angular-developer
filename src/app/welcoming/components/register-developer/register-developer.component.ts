a215fa8f8e7094714b8d29be0d6e09ef440331a3
Angel Velasquez <pcisavel@upc.edu.pe>
2024-04-03 12:36:21 -0500

    feat(register-developer): implemented template and behavior for register developer.

--- src/app/welcoming/components/register-developer/register-developer.component.ts
+++ src/app/welcoming/components/register-developer/register-developer.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-register-developer',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './register-developer.component.css'
 })
 export class RegisterDeveloperComponent {
+  developerForm = new FormGroup( {
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+  });
 
+  @Output() developerRegistered =
+    new EventEmitter<{ firstName: string, lastName: string}>();
+
+  onDeveloperRegistered(firstName: string, lastName: string): void {
+    this.developerRegistered.emit({firstName, lastName});
+  }
+
+  onSubmit(): void {
+    let firstName = this.developerForm.value.firstName != null ?
+      this.developerForm.value.firstName.toString() : '';
+    let lastName = this.developerForm.value.lastName != null ?
+      this.developerForm.value.lastName.toString() : '';
+    this.onDeveloperRegistered(firstName, lastName);
+  }
 }
