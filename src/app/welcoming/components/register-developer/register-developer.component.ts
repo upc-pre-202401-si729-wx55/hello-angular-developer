d1c6a7f389d502784ea4b227fcaea0f0c9eb9ef7
Angel Velasquez <pcisavel@upc.edu.pe>
2024-04-03 12:22:04 -0500

    feat(register-developer): added register developer feature.

---
+++ src/app/welcoming/components/register-developer/register-developer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-developer',
+  templateUrl: './register-developer.component.html',
+  styleUrl: './register-developer.component.css'
+})
+export class RegisterDeveloperComponent {
+
+}
